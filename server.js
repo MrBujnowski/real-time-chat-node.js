b9cd0bd85c688737b0859fbb76ce35a5f1712f6a
Antonín Bujnovský <120708132+MrBujnowski@users.noreply.github.com>
2024-09-11 06:54:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const io = require('socket.io')(3000)
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server); // Správné připojení socket.io
 
-const users = {}
+// Servírování statických souborů z aktuálního adresáře
+app.use(express.static(__dirname));
+
+const users = {};
 
 io.on('connection', socket => {
   socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected', name);
+  });
+
   socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
+    socket.broadcast.emit('chat-message', { message, name: users[socket.id] });
+  });
+
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
+    socket.broadcast.emit('user-disconnected', users[socket.id]);
+    delete users[socket.id];
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server běží na http://localhost:3000');
+});
